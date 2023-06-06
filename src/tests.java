4fd53ba60b89e88d5cbc793fb5239162669a2cf6
Alekss Možajevs <alekss.mozajevs@sk.lvt.lv>
2023-06-06 23:27:24 +0300

    Jautājumu nepareizas atbildes noteikšana un uzglabāšana

--- src/tests.java
+++ src/tests.java
@@ -1,8 +1,80 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class tests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int nepareizasAtbildes=0;	
+	String[] jautajumi = {"","","","","","","","","",""};
+	ArrayList<Integer> nepareiziJautajumi = new ArrayList<Integer>();
+	String atbilde;
+	
+	String[] jautajuma1Atbildes = {"a","b","c","d"};
+	atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[0], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+	if(atbilde.equalsIgnoreCase("a")) {
+		nepareizasAtbildes++;
+		nepareiziJautajumi.add(0);
+	}
+	
+		String[] jautajuma2Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(1);
+		}		
+		String[] jautajuma3Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(2);
+		}		
+		String[] jautajuma4Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}		
+		System.out.println(nepareiziJautajumi);
+		String[] jautajuma5Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}				
+		String[] jautajuma6Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}				
+		String[] jautajuma7Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}				
+		String[] jautajuma8Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}				
+		String[] jautajuma9Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}				
+		String[] jautajuma10Atbildes = {"","","",""};
+		atbilde =(String) JOptionPane.showInputDialog(null, jautajumi[1], "izvele",JOptionPane.PLAIN_MESSAGE, null, jautajuma1Atbildes, jautajuma1Atbildes[0]	);
+		if(atbilde.equalsIgnoreCase("a")) {
+			nepareizasAtbildes++;
+			nepareiziJautajumi.add(3);
+		}				
+		
+		
+
 
 	}
 
